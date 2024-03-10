59dbad38dcb76e4225ed94f38161841739b4cc08
Denyse Lima <denysepessoainha@gmail.com>
2024-03-10 15:52:23 -0300

    Javascript para abrir e fechar o menu completo

---
+++ script.js
@@ -0,0 +1,19 @@
+const botaoabrir = document.querySelector(".header-mobile > button")
+const botaofechar = document.querySelector(".header-mobile nav button")
+
+const body = document.querySelector("body")
+const nav = document.querySelector(".header-mobile nav")
+
+botaoabrir.addEventListener('click', abrirMenu)
+botaofechar.addEventListener('click', fecharMenu)
+
+
+function abrirMenu() {
+    body.classList.add("escurecer")
+    nav.classList.add("abrir")
+}
+
+function fecharMenu() {
+    body.classList.add("escurecer")
+    nav.classList.remove("abrir")
+}
